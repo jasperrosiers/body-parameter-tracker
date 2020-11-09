58382c6df1ff457a55fa7e86e88891e2c794967d
jasper rosiers <jasper.rosiers@ordina.be>
2020-11-09 11:14:46 +0100

    added native routing

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -1,11 +1,11 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {AzureHttpRouter} from "@nestjs/azure-func-http";
 
 export async function createApp(): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  
+  const app = await NestFactory.create(AppModule, new AzureHttpRouter());
+
   await app.init();
   return app;
 }
